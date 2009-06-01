dbf0cacd353d61b1bbc4f682715dd3c4e1751f7c
haffax <haffax@2b187f00-3b54-11de-b5fc-91815ed80701>
2009-06-01 20:16:15 +0000

    Properly stop main menu music when main menu closes.

--- mada/src/MadaMainMenuGameState.cpp
+++ mada/src/MadaMainMenuGameState.cpp
@@ -25,9 +25,10 @@
 
 namespace mada
 {
-	MainMenuGameState::MainMenuGameState() : mWindow(NULL)
+	MainMenuGameState::MainMenuGameState() : mWindow(NULL), mMusicFile()
 	{
 		mWindow = new MainMenuWindow();
+		mMusicFile = Mada::getSingleton().getGlobalParameter("music_main_menu");
 	}
 	//--------------------------------------------------------------------------------------------
 	MainMenuGameState::~MainMenuGameState()
@@ -37,8 +38,7 @@ namespace mada
 	//--------------------------------------------------------------------------------------------
 	void MainMenuGameState::resumeImpl()
 	{
-		String musicFile = Mada::getSingleton().getGlobalParameter("music_main_menu");
-		SoundManager::getSingleton().playSound2d(musicFile, true);
+		SoundManager::getSingleton().playSound2d(mMusicFile, true);
 
 		//mGuiManager->setBackgroundImage("bg_mainmenu.jpg");
 
@@ -47,6 +47,8 @@ namespace mada
 	//--------------------------------------------------------------------------------------------
 	void MainMenuGameState::suspendImpl()
 	{
+		mWindow->setVisible(false);
+		SoundManager::getSingleton().stopSound(mMusicFile);
 	}
 	//--------------------------------------------------------------------------------------------
 	void MainMenuGameState::run(Real timeSinceLastFrame)
