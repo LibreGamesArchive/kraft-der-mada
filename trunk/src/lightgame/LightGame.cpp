741fecebcc5d1cd6cdde2c8ae3b5ac5234c37e10
haffax@gmail.com <haffax@gmail.com@2b187f00-3b54-11de-b5fc-91815ed80701>
2009-11-12 23:32:02 +0000

    LightGame now has its own subclass of Game. Forgot to add actual files. Now done.

---
+++ trunk/src/lightgame/LightGame.cpp
@@ -0,0 +1,60 @@
+#pragma once
+
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Daniel Wickert
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdmadainc.h"
+
+#include "LightGame.h"
+
+namespace mada
+{
+	__mada_implement_class(LightGame, Game);
+	__mada_implement_singleton(LightGame);
+
+	LightGame::LightGame()
+	{
+		__mada_construct_singleton;
+	}
+
+	LightGame::~LightGame()
+	{
+		__mada_destruct_singleton;
+	}
+
+	void LightGame::open()
+	{
+		Game::open();
+	}
+
+	void LightGame::run()
+	{
+		m_gameState = mada::GameState::create();
+		mada::GameStateManager::getInstance()->registerGameState(m_gameState);
+
+		m_gameState->setName("START");
+		m_gameState->activate();
+
+		Game::run();
+	}
+
+	void LightGame::close()
+	{
+		Game::close();
+	}
+}
