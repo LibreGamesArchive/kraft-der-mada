e0ec31dc2a02bd354a42cb0548d447e3a052b3fe
haffax@gmail.com <haffax@gmail.com@2b187f00-3b54-11de-b5fc-91815ed80701>
2009-11-26 20:16:39 +0000

    Also update VC project files to reflect recent changes. This update is made to allow for a slight src directory restructuring.

---
+++ trunk/src/mada/game/core/CategoryManager.h
@@ -0,0 +1,41 @@
+#pragma once
+
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Daniel Wickert
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "game/base/Manager.h"
+
+namespace mada
+{
+	class CategoryManager : public Manager
+	{
+		__mada_declare_class(CategoryManager);
+		__mada_declare_singleton(CategoryManager);
+	public:
+		CategoryManager();
+		~CategoryManager();
+
+		void onActivate();
+		void onDeactivate();
+
+	private:
+		std::vector<String> m_categories;
+
+		void loadCategories();
+	};
+}
