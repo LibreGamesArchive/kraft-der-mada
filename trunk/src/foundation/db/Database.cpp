36e242f0f9a91ab7423df5e9cf6109b384420952
haffax@gmail.com <haffax@gmail.com@2b187f00-3b54-11de-b5fc-91815ed80701>
2009-11-26 20:09:21 +0000

    Add Db related classes. Mostly stubs still.

---
+++ trunk/src/foundation/db/Database.cpp
@@ -0,0 +1,133 @@
+#pragma once
+
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Daniel Wickert
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "stdmadainc.h"
+
+#include "db/Database.h"
+
+namespace mada
+{
+	__mada_implement_root_class(Database);
+
+	Database::Database() : m_path(), m_mode(ReadOnly), m_db(NULL)
+	{
+	}
+
+	Database::~Database()
+	{
+	}
+
+	void Database::setPath(const String& path)
+	{
+		m_path = path;
+	}
+
+	const String& Database::getPath() const
+	{
+		return m_path;
+	}
+
+	void Database::setAccessMode(AccessMode mode)
+	{
+		m_mode = mode;
+	}
+
+	Database::AccessMode Database::getAccessMode() const
+	{
+		return m_mode;
+	}
+
+	void Database::open()
+	{
+		mada_assert(!m_path.empty());
+		mada_assert(m_db == NULL);
+
+		int flags = 0;
+		if (m_mode == ReadOnly)
+		{
+			flags = SQLITE_OPEN_READONLY;
+		}
+		else if (m_mode == ReadWriteExisting)
+		{
+			flags = SQLITE_OPEN_READWRITE;
+		}
+		else if (m_mode == ReadWriteCreate)
+		{
+			flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+		}
+		flags |= SQLITE_OPEN_NOMUTEX;
+		flags |= SQLITE_OPEN_PRIVATECACHE;
+
+		if (sqlite3_open_v2(m_path.c_str(), &m_db, flags, NULL) != SQLITE_OK)
+		{
+			String msg = "db error in Database::open (sqlite3_open_v2): ";
+			msg += sqlite3_errmsg(m_db);
+			SysUtils::error(msg.c_str());
+		}
+	}
+
+	void Database::close()
+	{
+		mada_assert(m_db != NULL);
+		int resultCode = sqlite3_close(m_db);
+		if (resultCode == SQLITE_BUSY)
+		{
+			SysUtils::error("db error in Database::close (sqlite3_close): Database still has open statements.");
+		}
+		else if (resultCode != SQLITE_OK)
+		{
+			String msg = "db error in Database::close (sqlite3_close): ";
+			msg += sqlite3_errmsg(m_db);
+			SysUtils::error(msg.c_str());
+		}
+		m_db = NULL;
+	}
+
+	void Database::createTable(const String& tableName, const PropertyIdVector& propertyIds, bool dropExisting)
+	{
+		mada_assert(!tableName.empty());
+		mada_assert(!propertyIds.empty());
+	}
+
+	void Database::dropTable(const String& tableName)
+	{
+	}
+
+	Ptr<Table> Database::getTable(const String& tableName) const
+	{
+		return Ptr<Table>();
+	}
+
+	void Database::beginTransaction()
+	{
+	}
+
+	void Database::commitTransaction()
+	{
+	}
+
+	void Database::rollbackTransaction()
+	{
+	}
+
+	sqlite3* Database::_getDbHandle() const
+	{
+		return m_db;
+	}
+}
---
+++ trunk/src/foundation/db/DatabaseServer.h
@@ -0,0 +1,46 @@
+#pragma once
+
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Daniel Wickert
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "db/Database.h"
+
+namespace mada
+{
+	class DatabaseServer : public RefCounted
+	{
+		__mada_declare_class(DatabaseServer);
+		__mada_declare_singleton(DatabaseServer);
+	public:
+		DatabaseServer();
+		~DatabaseServer();
+
+		void open();
+		void close();
+		bool isOpen() const;
+
+		void openStaticDatabase(const String& path);
+		void closeStaticDatabase();
+		bool isStaticDatabaseOpen() const;
+		const Ptr<Database>& getStaticDatabase() const;
+
+	private:
+		bool m_isOpen;
+		Ptr<Database> m_staticDb;
+	};
+}
