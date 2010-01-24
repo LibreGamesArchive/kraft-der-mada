96682c4a698ba2a0344c98004895b4e2a345ce0f
timmeversmeyer <timmeversmeyer@2b187f00-3b54-11de-b5fc-91815ed80701>
2010-01-24 18:48:40 +0000

    introducing the sound feature

---
+++ src/foundation/sound/SoundServer.cpp
@@ -0,0 +1,72 @@
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Timm Eversmeyer
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdmadainc.h"
+
+#include "sound/SoundServer.h"
+#include "SoundServer.h"
+
+namespace mada
+{
+    __mada_implement_root_class(SoundServer);
+    __mada_implement_singleton(SoundServer);
+
+    SoundServer::SoundServer() : m_isOpen(false)
+    {
+
+    }
+
+    SoundServer::~SoundServer()
+    {
+        close();
+    }
+
+    void SoundServer::open()
+    {
+        mDevice = alcOpenDevice(NULL);
+
+        if(mDevice)
+        {
+            mContext = alcCreateContext(mDevice, NULL);
+            alcMakeContextCurrent(mContext);
+        }
+
+        mEAXsupported = alIsExtensionPresent("EAX2.0");
+
+        alGetError();
+
+        m_isOpen = true;
+    }
+
+    bool SoundServer::isOpen()
+    {
+        return m_isOpen;
+    }
+
+
+    void SoundServer::close()
+    {
+        if(m_isOpen)
+        {
+            alcMakeContextCurrent(NULL);
+            alcDestroyContext(mContext);
+            alcCloseDevice(mDevice);
+            m_isOpen = false;
+        }
+    }
+}
---
+++ src/foundation/sound/SoundServer.h
@@ -0,0 +1,46 @@
+#pragma once
+
+/*
+	This file is part of Kraft der Mada.
+	Copyright (c) 2009 Timm Eversmeyer
+
+    Kraft der Mada is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kraft der Mada is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Kraft der Mada. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <AL/alut.h>
+
+
+namespace mada
+{
+
+    class SoundServer : public RefCounted
+    {
+        __mada_declare_class(SoundServer);
+	__mada_declare_singleton(SoundServer);
+    public:
+        SoundServer();
+        ~SoundServer();
+
+        void open();
+        void close();
+
+        bool isOpen();
+    protected:
+        bool m_isOpen;
+        ALCdevice* mDevice;
+        ALCcontext* mContext;
+        bool mEAXsupported;
+    };
+
+}
